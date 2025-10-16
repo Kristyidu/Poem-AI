80086a900c985d550e419376f601d7bac8cfe015
kristyidu <xtyidu@yahoo.com>
2025-10-16 21:43:09 +0100

    Added more functionality and updated  HTML and CSS (Integration)

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Represents life, beauty, youth, fragility, and the present moment",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "8abadf7ae1834fobb53bf70t75fb8311";
+  let context =
+    "You are a great poet in England. You have the task of writing a very beautiful poem of not more than four lines and each line should have a  <br />.  Do not include a title to the poem. Sign the poem with 'Kristyidu' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an English poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
